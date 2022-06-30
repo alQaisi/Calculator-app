dd1d21b88f1a738a173e37a72219a0e4092b30ba
Ahmad Alqaisi <ahmedhamed1312@gmail.com>
2022-06-30 09:49:08 +0300

    adding localstorag

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
 const body=document.querySelector("body");
 const main=document.querySelector("main");
 function changeTheme(theme){
+   theme=theme==null?"theme1":theme;     
    body.className=theme;
    main.className=theme;
+   localStorage.setItem('theme',theme);
 }
+function setTheme(){
+    const theme=localStorage.getItem("theme");
+    const radioButton=document.querySelector(`#${theme}`);
+    radioButton?radioButton.checked=true:null;
+    changeTheme(theme);
+}
+setTheme();
 
 const prev=document.querySelector(".prev-operation");
 const result=document.querySelector(".result")
